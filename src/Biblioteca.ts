5e27d8f58f7c99241251f09d719a8185968d286e
SrNepo <srnep8@gmail.com>
2025-03-18 20:46:11 -0300

    criação da classe biblioteca, criação dos métodos adicionarIte() e buscarItem()

---
+++ src/Biblioteca.ts
@@ -0,0 +1,33 @@
+import type { Item } from "./Item";
+
+
+export class Biblioteca {
+    // Propiedades
+    itens:Item[];
+
+    // Construtor
+    constructor(){
+        this.itens = []
+    }
+
+    // Métodos
+
+    adicionarItem(item:Item): boolean {
+        if (!(this.itens.includes(item))) {
+            this.itens.push(item);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    buscarItens(termo:string): void {
+        this.itens.forEach(item => {
+            if (item.titulo === termo) {
+                console.log(item.exibirDetalhes());
+            }
+        }
+        );
+    }
+
+}
