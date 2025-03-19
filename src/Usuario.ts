c2ba9471d7a3c2f5b1b0dd223e28420adf03f3d0
SrNepo <srnep8@gmail.com>
2025-03-19 03:27:50 -0300

    Implementação dos getters e setters na classe Usuario e alteração de variáveis

--- src/Usuario.ts
+++ src/Usuario.ts
@@ -3,36 +3,77 @@ import { Item } from "./Item";
 export class Usuario {
     // propriedades privadas
     private _id:number;
-    private nome:string;
-    private email:string;
-    private telefone:string;
-    private itensEmprestados:Item[] = [];
-    private simbolo:symbol = Symbol("@email.com");
-    // depois voltamos aqui (parte 2: getters e setters)
+    private _nome:string;
+    private _email:string;
+    private _telefone:string;
+    private _itensEmprestados:Item[];
 
     // construtor
     constructor(id:number, nome:string, email:string, telefone:string
     ) {
         this._id = id;
-        this.nome = nome;
-        this.email = email;
-        this.telefone = telefone;
+        this._nome = nome;
+        this._email = email;
+        this._telefone = telefone;
+        this._itensEmprestados = []
     }
 
+    // getters e setters
+
+    get id(): number {
+        return this._id;
+    }
+    
+    get nome(): string {
+        return this._nome;
+    }
+
+    get email(): string {
+        return this._email;
+    }
+    
+    get telefone(): string {
+        return this._telefone;
+    }
+
+    get itensEmprestados(): Item[] {
+        return this._itensEmprestados;
+    }
+
+    set nome(nome: string) {
+        this._nome = nome;
+    }
+    
+    set email(email: string) {
+        if (!this.validarEmail(email)) {
+            throw new Error("Email inválido");
+        }
+        this._email = email;
+    }
+    
+    set telefone(telefone: string) {
+        this._telefone = telefone;
+    }
+    
+    private validarEmail(email: string): boolean {
+        const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        return regex.test(email);
+    }
+
+    // Métodos
 
     emprestarItem(item:Item): boolean {
-        if (!this.itensEmprestados.includes(item)) { // verifica se o item não tá no array
-            this.itensEmprestados.push(item); // coloca o item no array de itens emprestado
+        if (!this._itensEmprestados.includes(item)) { // verifica se o item não tá no array
+            this._itensEmprestados.push(item); // coloca o item no array de itens emprestado
             return true;
         } else {
             return false;
         }
     }
 
-
     devolverItem(item:Item): boolean {
-        if (this.itensEmprestados.includes(item)) { // verifica se o item tá no array
-            this.itensEmprestados = this.itensEmprestados.filter(
+        if (this._itensEmprestados.includes(item)) { // verifica se o item tá no array
+            this._itensEmprestados = this._itensEmprestados.filter(
                 item => item['id'] !== this._id
             ); // tira o item no array de itens emprestado
             return true; 
